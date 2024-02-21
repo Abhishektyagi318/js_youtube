f8485eaf86c12be2de98714b269ca6e9351b6eb2
Abhishek tyagi <90023512+Abhishektyagi318@users.noreply.github.com>
2024-02-21 15:42:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";   //treat all JS code as newer version
+// alert(3+3)   //we are using nodejs not browser to run JS
+console.log("Abhishek");
+
+let name = "Abhishek"
+let age  = 22
+let isLoggedIn = false
+let state;
+
+
+//number => 2 is to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+console.log(typeof"Abhishek");
+console.log(typeof age);
+
+console.log(typeof null);       //object               // important for interview point of view
+console.log(typeof undefined);  //undefined
